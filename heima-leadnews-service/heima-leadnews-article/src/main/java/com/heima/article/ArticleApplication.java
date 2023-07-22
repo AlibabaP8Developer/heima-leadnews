bf018dacac57d517ff032e4caab3e74c0714f38d
Edwin <1135365441@qq.com>
2023-07-22 23:16:35 +0800

    文章服务

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/ArticleApplication.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/ArticleApplication.java
@@ -3,13 +3,14 @@ package com.heima.article;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-
+@EnableSwagger2Doc
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.heima.article.mapper")
