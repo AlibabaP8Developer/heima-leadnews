d4c2a7460a2d87a7fb0da8a5075dd4c38c2bce34
Edwin <1135365441@qq.com>
2023-07-23 00:38:55 +0800

    自媒体服务创建

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/WemediaApplication.java
@@ -0,0 +1,18 @@
+package com.heima.wemedia;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableSwagger2Doc
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.heima.wemedia.mapper")
+public class WemediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WemediaApplication.class,args);
+    }
+}
