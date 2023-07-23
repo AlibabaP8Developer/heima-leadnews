8e93e8ed0d7822c29c601d98366e9deaff642d9e
Edwin <1135365441@qq.com>
2023-07-23 09:22:23 +0800

    自媒体拦截器定义

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/interceptor/WmTokenInterceptor.java
@@ -0,0 +1,47 @@
+package com.heima.wemedia.interceptor;
+
+import com.heima.model.wemedia.pojos.WmUser;
+import com.heima.utils.thread.WmThreadLocalUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WmTokenInterceptor implements HandlerInterceptor {
+
+    /**
+     * 前置
+     * 得到header中的用户信息，并且存入到当前线程中
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userId = request.getHeader("userId");
+        if (StringUtils.isNotBlank(userId)) {
+            // 存入到当前线程中
+            WmUser wmUser = new WmUser();
+            wmUser.setId(Integer.parseInt(userId));
+            WmThreadLocalUtil.setUser(wmUser);
+        }
+        return true;
+    }
+
+    /**
+     * 得到线程中的数据
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        WmThreadLocalUtil.clear();
+    }
+}
