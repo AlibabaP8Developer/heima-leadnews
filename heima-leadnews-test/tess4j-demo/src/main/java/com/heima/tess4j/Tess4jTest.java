494c52aed2fa015f8e366c6a8d8a62d28d444050
admin <1135365441@qq.com>
2023-07-24 08:45:56 +0800

    图片文字识别

--- heima-leadnews-test/tess4j-demo/src/main/java/com/heima/tess4j/Tess4jTest.java
+++ heima-leadnews-test/tess4j-demo/src/main/java/com/heima/tess4j/Tess4jTest.java
@@ -9,11 +9,11 @@ public class Tess4jTest {
     public static void main(String[] args) {
         Tesseract tesseract = new Tesseract();
         // 设置字体库路径
-        tesseract.setDatapath("/Users/lizhenghang/workspace/java/itheima/heima-leadnews/heima-leadnews-test/tess4j-demo/src/main/resources");
+        tesseract.setDatapath("D:\\developer\\workspace\\github\\heima-leadnews\\heima-leadnews-test\\tess4j-demo\\src\\main\\resources");
         // 设置语言
         tesseract.setLanguage("chi_sim");
 
-        File file = new File("/Users/lizhenghang/workspace/java/itheima/heima-leadnews/heima-leadnews-test/tess4j-demo/src/main/resources/3.jpg");
+        File file = new File("D:\\developer\\workspace\\github\\heima-leadnews\\heima-leadnews-test\\tess4j-demo\\src\\main\\resources\\2.png");
         try {
             // 识别图片
             String result = tesseract.doOCR(file);
