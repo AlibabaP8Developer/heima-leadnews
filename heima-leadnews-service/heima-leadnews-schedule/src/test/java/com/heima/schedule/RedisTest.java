51e056de66e69d0c43ac2bb82e90c59d9ccc0939
Edwin <1135365441@qq.com>
2023-11-12 09:22:08 +0800

    延迟队列-未来数据定时刷新

--- heima-leadnews-service/heima-leadnews-schedule/src/test/java/com/heima/schedule/RedisTest.java
+++ heima-leadnews-service/heima-leadnews-schedule/src/test/java/com/heima/schedule/RedisTest.java
@@ -1,11 +1,19 @@
 package com.heima.schedule;
 
+import com.alibaba.fastjson.JSON;
 import com.heima.common.redis.CacheService;
+import com.heima.model.schedule.dto.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.Set;
 
 @SpringBootTest(classes = ScheduleApplication.class)
 @RunWith(SpringRunner.class)
@@ -30,5 +38,45 @@ public class RedisTest {
         cacheService.zAdd("zset:add:002", "hello-002", 2000);
         cacheService.zAdd("zset:add:003", "hello-003", 3000);
         // 按照分值获取数据
+    }
+
+    @Test
+    public void testKey() {
+        Set<String> keys = cacheService.keys("future_");
+        System.out.println(keys);
+        Set<String> future = cacheService.scan("future_");
+        System.out.println(future);
+    }
+
+    @Test
+    public void testPiple() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            Task task = new Task();
+            task.setTaskType(1001);
+            task.setPriority(1);
+            task.setExecuteTime(new Date().getTime());
+            cacheService.lLeftPush("1001_1", JSON.toJSONString(task));
+        }
+        System.out.println("耗时：" + (System.currentTimeMillis() - start));
+    }
+
+    @Test
+    public void testPiple2() {
+        long start = System.currentTimeMillis();
+        cacheService.getstringRedisTemplate().executePipelined(new RedisCallback<Object>() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                for (int i = 0; i < 10000; i++) {
+                    Task task = new Task();
+                    task.setTaskType(1001);
+                    task.setPriority(1);
+                    task.setExecuteTime(new Date().getTime());
+                    redisConnection.lPush("100_1".getBytes(), JSON.toJSONString(task).getBytes());
+                }
+                return null;
+            }
+        });
+        System.out.println("使用管道技术执行10000次自增操作共耗时：" + (System.currentTimeMillis() - start) + "毫秒");
     }
 }
--- heima-leadnews-service/heima-leadnews-schedule/src/test/java/com/heima/schedule/TaskServiceImplTest.java
+++ heima-leadnews-service/heima-leadnews-schedule/src/test/java/com/heima/schedule/TaskServiceImplTest.java
@@ -1,21 +1,31 @@
 package com.heima.schedule;
 
+import com.heima.common.redis.CacheService;
 import com.heima.model.schedule.dto.Task;
 import com.heima.schedule.service.TaskService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
+import java.util.Set;
 
+import static com.heima.common.constants.ScheduleConstants.FUTURE;
+import static com.heima.common.constants.ScheduleConstants.TOPIC;
+
+@Slf4j
 @SpringBootTest(classes = ScheduleApplication.class)
 @RunWith(SpringRunner.class)
 public class TaskServiceImplTest {
 
     @Autowired
     private TaskService taskService;
+    @Autowired
+    private CacheService cacheService;
 
     @Test
     public void addTask() {
@@ -37,6 +47,24 @@ public class TaskServiceImplTest {
     public void testPoll() {
         Task task = taskService.poll(100, 50);
         System.out.println(task);
+    }
+
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void refresh() {
+        log.info("未来数据定时刷新——定时任务");
+        // 获取所有未来数据的集合key
+        Set<String> futureKeys = cacheService.scan(FUTURE + "*");
+        for (String futureKey : futureKeys) {
+            // 获取当前数据的key   topic
+            String topicKey = TOPIC + futureKey.split(FUTURE)[1];
+            // 按照key和分值查询符合条件的数据
+            Set<String> tasks = cacheService.zRangeByScore(futureKey, 0, System.currentTimeMillis());
+            // 同步数据
+            if (!tasks.isEmpty()) {
+                cacheService.refreshWithPipeline(futureKey, topicKey, tasks);
+                log.info("成功的将" + futureKey + "刷新到了" + topicKey);
+            }
+        }
     }
 
 }
